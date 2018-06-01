62f02e27311a588dcc67100e28deb8dda109cd4d
Zama Bandeira Braga <zama.braga@live.com>
2018-06-01 20:21:36 -0300

    add isFalse

--- src/index.js
+++ src/index.js
@@ -1,13 +1,70 @@
-module.exports = {
-  isTrue(param) {
+const booleanEnum = {
+  true: 'true',
+  false: 'false'
+}
+
+const booleanTranslate = {
+  [booleanEnum.true]: 'yes',
+  [booleanEnum.false]: 'no'
+}
+
+const booleanInt = {
+  [booleanEnum.true]: 1,
+  [booleanEnum.false]: 0
+}
+
+/**
+ * @class BooleanHelper
+ */
+class BooleanHelper {
+  /**
+   *
+   * @param {i18n} i18n instance
+   * @param {translateObj} object with boolean translate {true : 'yes', false : 'no'}
+   */
+  constructor({ i18n, translateObj }) {
+    this.i18n = i18n
+    this.booleanKeys = Object.assign(booleanTranslate, translateObj)
+  }
+
+  _isTrueOrFalse(param, value) {
     switch (typeof param) {
       case 'boolean':
-        return param === true
+        return param === value
       case 'string':
-        return param.toLowerCase() === 'true' || param === '1'
+        return (
+          param.toLowerCase() === booleanEnum[value] ||
+          param === booleanInt[value].toString() ||
+          (this.i18n === undefined &&
+            param.toLowerCase() ===
+              this.booleanKeys[booleanEnum[value]].toLowerCase()) ||
+          (this.i18n !== undefined &&
+            param.toLowerCase() ===
+              this.i18n.__(booleanEnum[value]).toLowerCase())
+        )
       case 'number':
-        return param === 1
+        return param === booleanInt[value]
     }
     return false
   }
+  /**
+   * Checks if a Boolean value is true, handling null by returning false.
+   * @param {object} param any object
+   * @returns {boolean} that boolean
+   */
+  isTrue(param) {
+    return this._isTrueOrFalse(param, true)
+  }
+  /**
+   * Checks if a Boolean value is false, handling null by returning false.
+   * @param {object} param any object
+   * @returns {boolean} that boolean
+   */
+  isFalse(param) {
+    return this._isTrueOrFalse(param, false)
+  }
+}
+
+module.exports = function(params) {
+  return new BooleanHelper(params || {})
 }
--- test/index.js
+++ test/index.js
@@ -1,38 +1,107 @@
 const expect = require('chai').expect
-const booleanUtils = require('./../src')
+const i18n = require('i18n')
+const ptBr = require('./locales/pt-BR.json')
+i18n.configure({
+  locales: ['pt-BR'],
+  defaultLocale: 'pt-BR',
+  directory: __dirname + '/locales'
+})
+
+const createBooleanUtils = require('./../src')
 
 describe('Boolean', () => {
   describe('#isTrue', () => {
     it('should return true when pass true', () => {
-      expect(booleanUtils.isTrue(true)).to.be.true
+      expect(createBooleanUtils().isTrue(true)).to.be.true
     })
     it('should return true when pass "true"', () => {
-      expect(booleanUtils.isTrue('true')).to.be.true
+      expect(createBooleanUtils().isTrue('true')).to.be.true
     })
     it('should return true when pass "TRUE"', () => {
-      expect(booleanUtils.isTrue('TRUE')).to.be.true
+      expect(createBooleanUtils().isTrue('TRUE')).to.be.true
+    })
+    it('should return true when pass "yes"', () => {
+      expect(createBooleanUtils().isTrue('yes')).to.be.true
+    })
+    it('should return true when pass "YES"', () => {
+      expect(createBooleanUtils().isTrue('YES')).to.be.true
+    })
+    it('should return true when pass "yes" in pt-BR', () => {
+      expect(createBooleanUtils({ translateObj: ptBr }).isTrue('sim')).to.be
+        .true
     })
     it('should return true when pass 1', () => {
-      expect(booleanUtils.isTrue(1)).to.be.true
+      expect(createBooleanUtils().isTrue(1)).to.be.true
     })
     it('should return true when pass "1"', () => {
-      expect(booleanUtils.isTrue('1')).to.be.true
+      expect(createBooleanUtils().isTrue('1')).to.be.true
     })
     it('should return false when pass false', () => {
-      expect(booleanUtils.isTrue(false)).to.be.false
+      expect(createBooleanUtils().isTrue(false)).to.be.false
     })
     it('should return false when pass "false" ', () => {
       var x = Boolean('false')
-      expect(booleanUtils.isTrue('false')).to.be.false
+      expect(createBooleanUtils().isTrue('false')).to.be.false
     })
     it('should return false when pass "FALSE" ', () => {
-      expect(booleanUtils.isTrue('FALSE')).to.be.false
+      expect(createBooleanUtils().isTrue('FALSE')).to.be.false
     })
     it('should return false when pass 0 ', () => {
-      expect(booleanUtils.isTrue(0)).to.be.false
+      expect(createBooleanUtils().isTrue(0)).to.be.false
     })
     it('should return false when pass "0" ', () => {
-      expect(booleanUtils.isTrue('0')).to.be.false
+      expect(createBooleanUtils().isTrue('0')).to.be.false
+    })
+    it('should return false when pass "false" in pt-BR', () => {
+      expect(createBooleanUtils({ translateObj: ptBr }).isTrue('não')).to.be
+        .false
+    })
+  })
+  describe('#isFalse', () => {
+    it('should return true when pass true', () => {
+      expect(createBooleanUtils().isFalse(true)).to.be.false
+    })
+    it('should return true when pass "true"', () => {
+      expect(createBooleanUtils().isFalse('true')).to.be.false
+    })
+    it('should return true when pass "TRUE"', () => {
+      expect(createBooleanUtils().isFalse('TRUE')).to.be.false
+    })
+    it('should return true when pass "yes"', () => {
+      expect(createBooleanUtils().isFalse('yes')).to.be.false
+    })
+    it('should return true when pass "YES"', () => {
+      expect(createBooleanUtils().isFalse('YES')).to.be.false
+    })
+    it('should return true when pass "yes" in pt-BR', () => {
+      expect(createBooleanUtils({ translateObj: ptBr }).isFalse('sim')).to.be
+        .false
+    })
+    it('should return true when pass 1', () => {
+      expect(createBooleanUtils().isFalse(1)).to.be.false
+    })
+    it('should return true when pass "1"', () => {
+      expect(createBooleanUtils().isFalse('1')).to.be.false
+    })
+    it('should return false when pass false', () => {
+      expect(createBooleanUtils().isFalse(false)).to.be.true
+    })
+    it('should return false when pass "false" ', () => {
+      var x = Boolean('false')
+      expect(createBooleanUtils().isFalse('false')).to.be.true
+    })
+    it('should return false when pass "FALSE" ', () => {
+      expect(createBooleanUtils().isFalse('FALSE')).to.be.true
+    })
+    it('should return false when pass 0 ', () => {
+      expect(createBooleanUtils().isFalse(0)).to.be.true
+    })
+    it('should return false when pass "0" ', () => {
+      expect(createBooleanUtils().isFalse('0')).to.be.true
+    })
+    it('should return false when pass "false" in pt-BR', () => {
+      expect(createBooleanUtils({ translateObj: ptBr }).isFalse('não')).to.be
+        .true
     })
   })
 })
