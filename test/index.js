1d683a73a256f124d4ab79b82327ee806eda2906
Zama Bandeira Braga <zama.braga@live.com>
2018-06-02 07:55:30 -0300

    refactor test

--- test/index.js
+++ test/index.js
@@ -1,4 +1,4 @@
-const expect = require('chai').expect
+const assert = require('chai').assert
 const i18n = require('i18n')
 const ptBr = require('./locales/pt-BR.json')
 i18n.configure({
@@ -7,191 +7,46 @@ i18n.configure({
   directory: __dirname + '/locales'
 })
 
-const createBooleanUtils = require('./../src')
+const booleanUtils = require('./../src').create()
+const trueScenarios = [true, 'true', 'TRUE', 'yes', 'YES', 1, '1']
+const falseScenarios = [false, 'false', 'FALSE', 'no', 'NO', 0, '0']
+
+const tests = (f, expectValue, scenarios) => {
+  scenarios.forEach(e => {
+    switch (typeof e) {
+      case 'string':
+        it(`should return ${expectValue.toString()} when pass "${e}"`, () => {
+          assert.equal(f.call(booleanUtils, e), expectValue)
+        })
+        break
+      default:
+        it(`should return ${expectValue.toString()} when pass ${e}`, () => {
+          assert.equal(f.call(booleanUtils, e), expectValue)
+        })
+        break
+    }
+  })
+
+  // it(`should return ${expectValue} when pass "yes" in pt-BR`, () => {
+  //   af(createBooleanUtils({ translateObj: ptBr }).isTrue('sim'))
+  // })
+}
 
 describe('Boolean', () => {
   describe('#isTrue', () => {
-    it('should return true when pass true', () => {
-      expect(createBooleanUtils().isTrue(true)).to.be.true
-    })
-    it('should return true when pass "true"', () => {
-      expect(createBooleanUtils().isTrue('true')).to.be.true
-    })
-    it('should return true when pass "TRUE"', () => {
-      expect(createBooleanUtils().isTrue('TRUE')).to.be.true
-    })
-    it('should return true when pass "yes"', () => {
-      expect(createBooleanUtils().isTrue('yes')).to.be.true
-    })
-    it('should return true when pass "YES"', () => {
-      expect(createBooleanUtils().isTrue('YES')).to.be.true
-    })
-    it('should return true when pass "yes" in pt-BR', () => {
-      expect(createBooleanUtils({ translateObj: ptBr }).isTrue('sim')).to.be
-        .true
-    })
-    it('should return true when pass 1', () => {
-      expect(createBooleanUtils().isTrue(1)).to.be.true
-    })
-    it('should return true when pass "1"', () => {
-      expect(createBooleanUtils().isTrue('1')).to.be.true
-    })
-    it('should return false when pass false', () => {
-      expect(createBooleanUtils().isTrue(false)).to.be.false
-    })
-    it('should return false when pass "false" ', () => {
-      expect(createBooleanUtils().isTrue('false')).to.be.false
-    })
-    it('should return false when pass "FALSE" ', () => {
-      expect(createBooleanUtils().isTrue('FALSE')).to.be.false
-    })
-    it('should return false when pass 0 ', () => {
-      expect(createBooleanUtils().isTrue(0)).to.be.false
-    })
-    it('should return false when pass "0" ', () => {
-      expect(createBooleanUtils().isTrue('0')).to.be.false
-    })
-    it('should return false when pass "false" in pt-BR', () => {
-      expect(createBooleanUtils({ translateObj: ptBr }).isTrue('não')).to.be
-        .false
-    })
+    tests(booleanUtils.isTrue, true, trueScenarios)
+    tests(booleanUtils.isTrue, false, falseScenarios)
   })
   describe('#isFalse', () => {
-    it('should return false when pass true', () => {
-      expect(createBooleanUtils().isFalse(true)).to.be.false
-    })
-    it('should return false when pass "true"', () => {
-      expect(createBooleanUtils().isFalse('true')).to.be.false
-    })
-    it('should return false when pass "TRUE"', () => {
-      expect(createBooleanUtils().isFalse('TRUE')).to.be.false
-    })
-    it('should return false when pass "yes"', () => {
-      expect(createBooleanUtils().isFalse('yes')).to.be.false
-    })
-    it('should return false when pass "YES"', () => {
-      expect(createBooleanUtils().isFalse('YES')).to.be.false
-    })
-    it('should return false when pass "yes" in pt-BR', () => {
-      expect(createBooleanUtils({ translateObj: ptBr }).isFalse('sim')).to.be
-        .false
-    })
-    it('should return false when pass 1', () => {
-      expect(createBooleanUtils().isFalse(1)).to.be.false
-    })
-    it('should return false when pass "1"', () => {
-      expect(createBooleanUtils().isFalse('1')).to.be.false
-    })
-    it('should return true when pass false', () => {
-      expect(createBooleanUtils().isFalse(false)).to.be.true
-    })
-    it('should return true when pass "false" ', () => {
-      expect(createBooleanUtils().isFalse('false')).to.be.true
-    })
-    it('should return true when pass "FALSE" ', () => {
-      expect(createBooleanUtils().isFalse('FALSE')).to.be.true
-    })
-    it('should return true when pass 0 ', () => {
-      expect(createBooleanUtils().isFalse(0)).to.be.true
-    })
-    it('should return true when pass "0" ', () => {
-      expect(createBooleanUtils().isFalse('0')).to.be.true
-    })
-    it('should return true when pass "false" in pt-BR', () => {
-      expect(createBooleanUtils({ translateObj: ptBr }).isFalse('não')).to.be
-        .true
-    })
+    tests(booleanUtils.isFalse, false, trueScenarios)
+    tests(booleanUtils.isFalse, true, falseScenarios)
   })
   describe('#isNotTrue', () => {
-    it('should return false when pass true', () => {
-      expect(createBooleanUtils().isNotTrue(true)).to.be.false
-    })
-    it('should return false when pass "true"', () => {
-      expect(createBooleanUtils().isNotTrue('true')).to.be.false
-    })
-    it('should return false when pass "TRUE"', () => {
-      expect(createBooleanUtils().isNotTrue('TRUE')).to.be.false
-    })
-    it('should return false when pass "yes"', () => {
-      expect(createBooleanUtils().isNotTrue('yes')).to.be.false
-    })
-    it('should return false when pass "YES"', () => {
-      expect(createBooleanUtils().isNotTrue('YES')).to.be.false
-    })
-    it('should return false when pass "yes" in pt-BR', () => {
-      expect(createBooleanUtils({ translateObj: ptBr }).isNotTrue('sim')).to.be
-        .false
-    })
-    it('should return false when pass 1', () => {
-      expect(createBooleanUtils().isNotTrue(1)).to.be.false
-    })
-    it('should return false when pass "1"', () => {
-      expect(createBooleanUtils().isNotTrue('1')).to.be.false
-    })
-    it('should return true when pass false', () => {
-      expect(createBooleanUtils().isNotTrue(false)).to.be.true
-    })
-    it('should return true when pass "false" ', () => {
-      expect(createBooleanUtils().isNotTrue('false')).to.be.true
-    })
-    it('should return true when pass "FALSE" ', () => {
-      expect(createBooleanUtils().isNotTrue('FALSE')).to.be.true
-    })
-    it('should return true when pass 0 ', () => {
-      expect(createBooleanUtils().isNotTrue(0)).to.be.true
-    })
-    it('should return true when pass "0" ', () => {
-      expect(createBooleanUtils().isNotTrue('0')).to.be.true
-    })
-    it('should return true when pass "false" in pt-BR', () => {
-      expect(createBooleanUtils({ translateObj: ptBr }).isNotTrue('não')).to.be
-        .true
-    })
+    tests(booleanUtils.isNotTrue, false, trueScenarios)
+    tests(booleanUtils.isNotTrue, true, falseScenarios)
   })
   describe('#isNotFalse', () => {
-    it('should return true when pass true', () => {
-      expect(createBooleanUtils().isNotFalse(true)).to.be.true
-    })
-    it('should return true when pass "true"', () => {
-      expect(createBooleanUtils().isNotFalse('true')).to.be.true
-    })
-    it('should return true when pass "TRUE"', () => {
-      expect(createBooleanUtils().isNotFalse('TRUE')).to.be.true
-    })
-    it('should return true when pass "yes"', () => {
-      expect(createBooleanUtils().isNotFalse('yes')).to.be.true
-    })
-    it('should return true when pass "YES"', () => {
-      expect(createBooleanUtils().isNotFalse('YES')).to.be.true
-    })
-    it('should return true when pass "yes" in pt-BR', () => {
-      expect(createBooleanUtils({ translateObj: ptBr }).isNotFalse('sim')).to.be
-        .true
-    })
-    it('should return true when pass 1', () => {
-      expect(createBooleanUtils().isNotFalse(1)).to.be.true
-    })
-    it('should return true when pass "1"', () => {
-      expect(createBooleanUtils().isNotFalse('1')).to.be.true
-    })
-    it('should return false when pass false', () => {
-      expect(createBooleanUtils().isNotFalse(false)).to.be.false
-    })
-    it('should return false when pass "false" ', () => {
-      expect(createBooleanUtils().isNotFalse('false')).to.be.false
-    })
-    it('should return false when pass "FALSE" ', () => {
-      expect(createBooleanUtils().isNotFalse('FALSE')).to.be.false
-    })
-    it('should return false when pass 0 ', () => {
-      expect(createBooleanUtils().isNotFalse(0)).to.be.false
-    })
-    it('should return false when pass "0" ', () => {
-      expect(createBooleanUtils().isNotFalse('0')).to.be.false
-    })
-    it('should return false when pass "false" in pt-BR', () => {
-      expect(createBooleanUtils({ translateObj: ptBr }).isNotFalse('não')).to.be
-        .false
-    })
+    tests(booleanUtils.isNotFalse, true, trueScenarios)
+    tests(booleanUtils.isNotFalse, false, falseScenarios)
   })
 })
